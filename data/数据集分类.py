73e13b41b29b0c868c5a22463d65397b7f0157f8
worldfracture <a1310301641@gmail.com>
2024-03-28 16:23:32 +0800

    Merge remote-tracking branch 'origin/master'

--- data/数据集分类.py
+++ data/数据集分类.py
@@ -1,15 +1,15 @@
 import os
 import cv2
 
-labels = open("train/label.txt", "r")
+labels = open("test/label.txt", "r")
 label = labels.read().split(',')
 print(len(label))
 
 # path = 'test/test/'
-path = 'train/'
+path = 'test/'
 for cnt in range(len(label)):
     image_path = (path + str(cnt) + '.png')
     img = cv2.imread(image_path)
     # 根据图片对应的标签分类到对应的文件夹下：
-    cv2.imwrite('emnist/Train_png/' + chr(int(label[cnt])+64) + '/' + str(cnt) + '.png', img)
+    cv2.imwrite('emnist/Test_png/' + label[cnt] + '/' + str(cnt) + '.png', img)
     cnt += 1
